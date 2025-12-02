443d7184490c77143c01107e80e44937069addb1
Jan Martinek <jmartinek11@gmail.com>
2025-12-02 23:18:10 +0100

    feat(SD.Cryptography): Add implementation

---
+++ src/SharpDb.Cryptography/AesEncrypt.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+
+namespace SharpDb.Cryptography;
+
+/// <summary>
+/// Utility class for AES encryption and decryption.
+/// </summary>
+/// <remarks>!!! DO NOT USE OUTSIDE OF YOUR DATABASE PROJECTS !!!</remarks>
+public static class AesEncryption
+{
+    public static byte[] DeterministicEncrypt(byte[] bytes, byte[] key)
+    {
+        using var aes = Aes.Create();
+        aes.Key = key;
+        aes.IV = MD5.HashData(bytes)[..16];
+        aes.Mode = CipherMode.CBC;
+        aes.Padding = PaddingMode.PKCS7;
+
+        using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+        using var msEncrypt = new MemoryStream();
+        using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+        using var swEncrypt = new StreamWriter(csEncrypt);
+
+        msEncrypt.Write(aes.IV, 0, aes.IV.Length);
+        csEncrypt.Write(bytes);
+        csEncrypt.FlushFinalBlock();
+
+        return msEncrypt.ToArray();
+    }
+
+    public static byte[] Decrypt(byte[] cipherBytes, byte[] key)
+    {
+        using var aes = Aes.Create();
+        aes.Key = key;
+        aes.IV = cipherBytes[..16];
+        aes.Mode = CipherMode.CBC;
+        aes.Padding = PaddingMode.PKCS7;
+
+        using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+        using var msDecrypt = new MemoryStream(cipherBytes, 16, cipherBytes.Length - 16);
+        using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+        using var outStream = new MemoryStream();
+
+        try
+        {
+            csDecrypt.CopyTo(outStream);
+            return outStream.ToArray();
+        }
+        catch (CryptographicException e)
+        {
+            throw new CryptographicException("Decryption failed", e);
+        }
+    }
+}
---
+++ src/SharpDb.Cryptography/Hash.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SharpDb.Cryptography;
+
+/// <summary>
+/// Utility class for hashing data.
+/// </summary>
+/// <remarks>!!! DO NOT USE OUTSIDE OF YOUR DATABASE PROJECTS !!!</remarks>
+public static class Hash
+{
+    public static string ConvertToSha256(string input)
+    {
+        return ConvertToSha256(input, Encoding.UTF8);
+    }
+
+    public static string ConvertToSha256(string input, Encoding encoding)
+    {
+        byte[] inputBytes = encoding.GetBytes(input);
+        byte[] hashBytes = SHA256.HashData(inputBytes);
+        StringBuilder output = new();
+        foreach (byte b in hashBytes)
+        {
+            output.Append(b.ToString("x2"));
+        }
+        return output.ToString();
+    }
+
+    public static string ConvertToMd5(string input)
+    {
+        return ConvertToMd5(input, Encoding.UTF8);
+    }
+
+    public static string ConvertToMd5(string input, Encoding encoding)
+    {
+        byte[] inputBytes = encoding.GetBytes(input);
+        byte[] hashBytes = MD5.HashData(inputBytes);
+        StringBuilder output = new();
+        foreach (byte b in hashBytes)
+        {
+            output.Append(b.ToString("x2"));
+        }
+        return output.ToString();
+    }
+}
