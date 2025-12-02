7d023db3ee1c99228085f854c9782d372565e0b7
Jan Martinek <jmartinek11@gmail.com>
2025-12-02 23:39:58 +0100

    feat(SD.EntityFrameworkCore): Add a SQL only wrapper around the database facade to hide some methods
    - reason: We may not necesarrily want to allow users to see the EFC
    directly. This allows for easier infrastructure swap if needed in the
    future.
    - proposal: Add a wrapper, hiding the database implementation from the
    users, but still allowing access to SQL methods.

---
+++ src/SharpDb.EntityFrameworkCore/EfcSqlRunner.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace SharpDb.EntityFrameworkCore;
+
+public sealed class EfcSqlRunner : ISqlRunner
+{
+    private readonly DatabaseFacade _db;
+    private DbConnectionInfo? _connectionInfo;
+
+    public EfcSqlRunner(DatabaseFacade db)
+    {
+        ArgumentNullException.ThrowIfNull(db, nameof(db));
+        _db = db;
+        _connectionInfo = null;
+    }
+
+    public DbConnectionInfo? GetConnectionInfo()
+    {
+        if (_connectionInfo is null)
+        {
+            if (_db.GetConnectionString() is string connectionString)
+            {
+                _connectionInfo = DbConnectionInfo.FromConnectionString(connectionString);
+            }
+        }
+        return _connectionInfo;
+    }
+
+    public ValueTask<DbQueryResult<T>> SqlSingleAsync<T>(FormattableString sql, Func<DbDataReader, T> reader)
+        => _db.SqlSingleAsync(sql, reader);
+
+    public ValueTask<DbQueryResult<T>> RawSqlSingleAsync<T>(string sql, Func<DbDataReader, T> reader, params DbParameter[] parameters)
+        => _db.RawSqlSingleAsync(sql, reader, parameters);
+
+    public ValueTask<DbQueryResult<T[]>> SqlManyAsync<T>(FormattableString sql, Func<DbDataReader, T> reader)
+        => _db.SqlManyAsync(sql, reader);
+
+    public ValueTask<DbQueryResult<T[]>> RawSqlManyAsync<T>(string sql, Func<DbDataReader, T> reader, params DbParameter[] parameters)
+        => _db.RawSqlManyAsync(sql, reader, parameters);
+
+    public ValueTask<DbExecResult> SqlExecuteAsync(FormattableString sql)
+        => _db.SqlExecuteAsync(sql);
+
+    public ValueTask<DbExecResult> RawSqlExecuteAsync(string sql, params DbParameter[] parameters)
+        => _db.RawSqlExecuteAsync(sql, parameters);
+
+    public ValueTask<DbQueryResult<T>> StoredProcedureSingleAsync<T>(string procedureName, Func<DbDataReader, T> reader, params DbParameter[] parameters)
+        => _db.StoredProcedureSingleAsync(procedureName, reader, parameters);
+
+    public ValueTask<DbQueryResult<T[]>> StoredProcedureManyAsync<T>(string procedureName, Func<DbDataReader, T> reader, params DbParameter[] parameters)
+        => _db.StoredProcedureManyAsync(procedureName, reader, parameters);
+
+    public ValueTask<DbExecResult> StoredProcedureExecuteAsync(string procedureName, params DbParameter[] parameters)
+        => _db.StoredProcedureExecuteAsync(procedureName, parameters);
+}
