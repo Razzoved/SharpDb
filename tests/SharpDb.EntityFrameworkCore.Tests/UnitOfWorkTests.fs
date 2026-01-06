ae05242b268e61718f39cd5d9d207f8a621a253d
Jan Martinek <jmartinek11@gmail.com>
2026-01-06 13:58:44 +0100

    test(SD.EntityFrameworkCore): Change UoW tests due to DbContext ownership specification

--- tests/SharpDb.EntityFrameworkCore.Tests/UnitOfWorkTests.fs
+++ tests/SharpDb.EntityFrameworkCore.Tests/UnitOfWorkTests.fs
@@ -2,6 +2,7 @@
 
 open SharpDb.EntityFrameworkCore
 open System
+open System.Reflection
 open System.Threading.Tasks
 open Microsoft.Data.Sqlite
 open Microsoft.EntityFrameworkCore
@@ -20,214 +21,209 @@ module UnitOfWorkTests =
             modelBuilder.Entity<DummyEntity>().HasKey("Id") |> ignore
             modelBuilder.Entity<DummyEntity>().Property(fun e -> e.Id).ValueGeneratedOnAdd().HasColumnName("id") |> ignore
 
-    type DummyUnitOfWork(ctx: DbContext) =
-        inherit UnitOfWork(ctx)
+    type DummyUnitOfWork(ctxFactory: IDbContextFactory<DummyDbContext>) =
+        inherit UnitOfWork<DummyDbContext>(ctxFactory)
+        let rec findContextField(obj, t: Type) =
+            let field = t.GetField("_dbContext", BindingFlags.NonPublic ||| BindingFlags.Instance)
+            if field <> null then
+                field.GetValue(obj) :?> DummyDbContext
+            else
+                let baseType = t.BaseType
+                if baseType <> null then
+                    findContextField(obj, baseType)
+                else
+                    failwith "Unable to access _dbContext field"
+        member this.PrivateContext = findContextField(this, this.GetType())
 
+    type InMemoryContextFactory() =
+        interface IDbContextFactory<DummyDbContext> with
+            member _.CreateDbContext() =
+                let options = DbContextOptionsBuilder<DummyDbContext>()
+                                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                                .Options
+                let ctx = new DummyDbContext(options)
+                ctx.Database.EnsureCreated() |> ignore
+                ctx
 
-    let createContext() =
-        let options = DbContextOptionsBuilder<DummyDbContext>()
-                        .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                        .Options
-        let ctx = new DummyDbContext(options)
-        ctx.Database.EnsureCreated() |> ignore
-        ctx
-
-    let createContextSqlite() =
-        let conn = new SqliteConnection($"Data Source={Guid.NewGuid()};mode=memory;cache=shared;")
-        conn.Open()
-        let options = DbContextOptionsBuilder<DummyDbContext>()
-                        .UseSqlite(conn)
-                        .Options
-        let ctx = new DummyDbContext(options)
-        ctx.Database.EnsureCreated() |> ignore
-        ctx, conn
+    type SqliteContextFactory() =
+        let connection = new SqliteConnection($"Data Source={Guid.NewGuid()};mode=memory;cache=shared;")
+        interface System.IDisposable with
+            member _.Dispose() =
+                if connection.State = System.Data.ConnectionState.Open then
+                    connection.Close()
+                connection.Dispose()
+        interface IDbContextFactory<DummyDbContext> with
+            member _.CreateDbContext() =
+                if connection.State <> System.Data.ConnectionState.Open then
+                    connection.Open()
+                let options = DbContextOptionsBuilder<DummyDbContext>()
+                                .UseSqlite(connection)
+                                .Options
+                let ctx = new DummyDbContext(options)
+                ctx.Database.EnsureCreated() |> ignore
+                ctx
 
     [<Fact>]
     let ``Attach sets entity state to Unchanged if Detached`` () =
-        use ctx = createContext()
-        let uow = DummyUnitOfWork(ctx)
+        let dbContextFactory = new InMemoryContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
         let entity = DummyEntity()
-        ctx.Add(entity) |> ignore
-        ctx.Entry(entity).State <- EntityState.Detached
+        uow.PrivateContext.Add(entity) |> ignore
+        uow.PrivateContext.Entry(entity).State <- EntityState.Detached
         uow.Attach(entity)
-        Assert.Equal(EntityState.Unchanged, ctx.Entry(entity).State)
+        Assert.Equal(EntityState.Unchanged, uow.PrivateContext.Entry(entity).State)
 
     [<Fact>]
     let ``Attach does not change entity state if not Detached`` () =
-        use ctx = createContext()
-        let uow = DummyUnitOfWork(ctx)
+        let dbContextFactory = new InMemoryContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
         let entity = DummyEntity()
-        ctx.Add(entity) |> ignore
-        ctx.Entry(entity).State <- EntityState.Modified
+        uow.PrivateContext.Add(entity) |> ignore
+        uow.PrivateContext.Entry(entity).State <- EntityState.Modified
         uow.Attach(entity)
-        Assert.Equal(EntityState.Modified, ctx.Entry(entity).State)
+        Assert.Equal(EntityState.Modified, uow.PrivateContext.Entry(entity).State)
 
     [<Fact>]
     let ``Attach adds entity if not tracked`` () =
-        use ctx = createContext()
-        let uow = DummyUnitOfWork(ctx)
+        let dbContextFactory = new InMemoryContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
         let entity = DummyEntity()
-        Assert.Equal(EntityState.Detached, ctx.Entry(entity).State)
+        Assert.Equal(EntityState.Detached, uow.PrivateContext.Entry(entity).State)
         uow.Attach(entity)
-        Assert.Equal(EntityState.Unchanged, ctx.Entry(entity).State)
+        Assert.Equal(EntityState.Unchanged, uow.PrivateContext.Entry(entity).State)
 
     [<Fact>]
     let ``Detach sets entity state to Detached if not Detached`` () =
-        use ctx = createContext()
-        let uow = DummyUnitOfWork(ctx)
+        let dbContextFactory = new InMemoryContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
         let entity = DummyEntity()
-        ctx.Add(entity) |> ignore
+        uow.PrivateContext.Add(entity) |> ignore
         uow.Detach(entity)
-        Assert.Equal(EntityState.Detached, ctx.Entry(entity).State)
+        Assert.Equal(EntityState.Detached, uow.PrivateContext.Entry(entity).State)
 
     [<Fact>]
     let ``SaveChanges returns affected rows`` () =
-        use ctx = createContext()
-        let uow = DummyUnitOfWork(ctx)
+        let dbContextFactory = new InMemoryContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
         let entity = DummyEntity()
-        ctx.Add(entity) |> ignore
+        uow.PrivateContext.Add(entity) |> ignore
         let affected = uow.SaveChanges()
         Assert.Equal(1, affected)
 
     [<Fact>]
     let ``SaveChangesAsync returns affected rows`` () =
-        use ctx = createContext()
-        let uow = DummyUnitOfWork(ctx)
+        let dbContextFactory = new InMemoryContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
         let entity = DummyEntity()
-        ctx.Add(entity) |> ignore
+        uow.PrivateContext.Add(entity) |> ignore
         let affected = uow.SaveChangesAsync().Result
         Assert.Equal(1, affected)
 
     [<Fact>]
     let ``DiscardChanges clears change tracker`` () =
-        use ctx = createContext()
-        let uow = DummyUnitOfWork(ctx)
+        let dbContextFactory = new InMemoryContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
         let entity = DummyEntity()
-        ctx.Add(entity) |> ignore
+        uow.PrivateContext.Add(entity) |> ignore
         uow.DiscardChanges()
-        Assert.Empty(ctx.ChangeTracker.Entries())
+        Assert.Empty(uow.PrivateContext.ChangeTracker.Entries())
 
     [<Fact>]
     let ``InTransaction executes action and returns success`` () =
-        let ctx, conn = createContextSqlite()
-        try
-            let uow = DummyUnitOfWork(ctx)
-            let entity = DummyEntity()
-            entity.Name <- "Test"
-            Assert.False(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
-            let result = uow.InTransaction(fun () ->
-                ctx.Add(entity) |> ignore
-                uow.SaveChanges() |> ignore
-            )
-            Assert.True(result.IsSuccess)
-            Assert.True(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
-        finally
-            ctx.Dispose()
-            conn.Close()
-            conn.Dispose()
+        use dbContextFactory = new SqliteContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
+        let entity = DummyEntity()
+        entity.Name <- "Test"
+        Assert.False(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
+        let result = uow.InTransaction(fun () ->
+            uow.PrivateContext.Add(entity) |> ignore
+            uow.SaveChanges() |> ignore
+        )
+        Assert.True(result.IsSuccess)
+        Assert.True(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
 
     [<Fact>]
     let ``InTransactionAsync executes action and returns success`` () : Task =
         task {
-            let ctx, conn = createContextSqlite()
-            try
-                let uow = DummyUnitOfWork(ctx)
-                let entity = DummyEntity()
-                entity.Name <- "Test"
-                Assert.False(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
-                let! result = uow.InTransactionAsync(fun () -> task {
-                    ctx.Add(entity) |> ignore
-                    uow.SaveChangesAsync() |> ignore
-                })
-                Assert.True(result.IsSuccess)
-                Assert.True(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
-            finally
-                ctx.Dispose()
-                conn.Close()
-                conn.Dispose()
+            use dbContextFactory = new SqliteContextFactory()
+            use uow = new DummyUnitOfWork(dbContextFactory)
+            let entity = DummyEntity()
+            entity.Name <- "Test"
+            Assert.False(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
+            let! result = uow.InTransactionAsync(fun () -> task {
+                uow.PrivateContext.Add(entity) |> ignore
+                uow.SaveChangesAsync() |> ignore
+            })
+            Assert.True(result.IsSuccess)
+            Assert.True(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
         }
 
     [<Fact>]
     let ``InTransaction rolls back on exception`` () =
-        let ctx, conn = createContextSqlite()
-        try
-            let uow = DummyUnitOfWork(ctx)
-            let entity = DummyEntity()
-            entity.Name <- "Test"
-            Assert.False(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
-            let result = uow.InTransaction(fun () ->
-                ctx.Add(entity) |> ignore
-                uow.SaveChanges() |> ignore
-                raise (Exception("Test exception"))
-            )
-            Assert.False(result.IsSuccess)
-            Assert.False(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
-        finally
-            ctx.Dispose()
-            conn.Close()
-            conn.Dispose()
+        use dbContextFactory = new SqliteContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
+        let entity = DummyEntity()
+        entity.Name <- "Test"
+        Assert.False(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
+        let result = uow.InTransaction(fun () ->
+            uow.PrivateContext.Add(entity) |> ignore
+            uow.SaveChanges() |> ignore
+            raise (Exception("Test exception"))
+        )
+        Assert.False(result.IsSuccess)
+        Assert.False(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = entity.Name) |> Async.AwaitTask |> Async.RunSynchronously)
 
     [<Fact>]
     let ``InTransaction rolls back to previous state on exception`` () =
-        let ctx, conn = createContextSqlite()
-        try
-            let uow = DummyUnitOfWork(ctx)
-            let entity = DummyEntity()
-            entity.Name <- "Test"
-            Assert.Empty(ctx.Set<DummyEntity>().Local) |> ignore
-            ctx.Add(entity) |> ignore
-            Assert.Single(ctx.Set<DummyEntity>().Local) |> ignore
-            let result = uow.InTransaction(fun () ->
-                Assert.Equal(ctx.Entry(entity).State, EntityState.Added)
-                uow.SaveChanges() |> ignore
-                Assert.Equal(ctx.Entry(entity).State, EntityState.Unchanged)
-                Assert.True(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Test") |> Async.AwaitTask |> Async.RunSynchronously)
-                raise (Exception("Test exception"))
-            )
-            Assert.False(result.IsSuccess)
-            Assert.False(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Test") |> Async.AwaitTask |> Async.RunSynchronously)
-            Assert.Single(ctx.Set<DummyEntity>().Local) |> ignore
-            Assert.Equal(ctx.Entry(entity).State, EntityState.Added)
-            Assert.Equal("Test", entity.Name)
-        finally
-            ctx.Dispose()
-            conn.Close()
-            conn.Dispose()
+        use dbContextFactory = new SqliteContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
+        let entity = DummyEntity()
+        entity.Name <- "Test"
+        Assert.Empty(uow.PrivateContext.Set<DummyEntity>().Local) |> ignore
+        uow.PrivateContext.Add(entity) |> ignore
+        Assert.Single(uow.PrivateContext.Set<DummyEntity>().Local) |> ignore
+        let result = uow.InTransaction(fun () ->
+            Assert.Equal(uow.PrivateContext.Entry(entity).State, EntityState.Added)
+            uow.SaveChanges() |> ignore
+            Assert.Equal(uow.PrivateContext.Entry(entity).State, EntityState.Unchanged)
+            Assert.True(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Test") |> Async.AwaitTask |> Async.RunSynchronously)
+            raise (Exception("Test exception"))
+        )
+        Assert.False(result.IsSuccess)
+        Assert.False(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Test") |> Async.AwaitTask |> Async.RunSynchronously)
+        Assert.Single(uow.PrivateContext.Set<DummyEntity>().Local) |> ignore
+        Assert.Equal(uow.PrivateContext.Entry(entity).State, EntityState.Added)
+        Assert.Equal("Test", entity.Name)
 
     [<Fact>]
     let ``InTransaction rolls back to previous state and values on exception`` () =
-        let ctx, conn = createContextSqlite()
-        try
-            let uow = DummyUnitOfWork(ctx)
-            let entity = DummyEntity()
-            entity.Name <- "Test"
-            Assert.Empty(ctx.Set<DummyEntity>().Local) |> ignore
-            ctx.Add(entity) |> ignore
-            Assert.Single(ctx.Set<DummyEntity>().Local) |> ignore
-            let result = uow.InTransaction(fun () ->
-                Assert.Equal(ctx.Entry(entity).State, EntityState.Added)
-                entity.Name <- "Changed1"
-                uow.SaveChanges() |> ignore
-                Assert.True(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Changed1") |> Async.AwaitTask |> Async.RunSynchronously)
-                Assert.Equal(ctx.Entry(entity).State, EntityState.Unchanged)
-                entity.Name <- "Changed2"
-                ctx.Update(entity) |> ignore
-                Assert.Equal(ctx.Entry(entity).State, EntityState.Modified)
-                uow.SaveChanges() |> ignore
-                Assert.Equal(ctx.Entry(entity).State, EntityState.Unchanged)
-                Assert.True(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Changed2") |> Async.AwaitTask |> Async.RunSynchronously)
-                raise (Exception("Test exception"))
-            )
-            Assert.False(result.IsSuccess)
-            Assert.False(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Changed2") |> Async.AwaitTask |> Async.RunSynchronously)
-            Assert.False(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Changed1") |> Async.AwaitTask |> Async.RunSynchronously)
-            Assert.False(ctx.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Test") |> Async.AwaitTask |> Async.RunSynchronously)
-            Assert.Single(ctx.Set<DummyEntity>().Local) |> ignore
-            Assert.Equal(ctx.Entry(entity).State, EntityState.Added)
-            Assert.Equal("Test", entity.Name)
-        finally
-            ctx.Dispose()
-            conn.Close()
-            conn.Dispose()
+        use dbContextFactory = new SqliteContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
+        let entity = DummyEntity()
+        entity.Name <- "Test"
+        Assert.Empty(uow.PrivateContext.Set<DummyEntity>().Local) |> ignore
+        uow.PrivateContext.Add(entity) |> ignore
+        Assert.Single(uow.PrivateContext.Set<DummyEntity>().Local) |> ignore
+        let result = uow.InTransaction(fun () ->
+            Assert.Equal(uow.PrivateContext.Entry(entity).State, EntityState.Added)
+            entity.Name <- "Changed1"
+            uow.SaveChanges() |> ignore
+            Assert.True(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Changed1") |> Async.AwaitTask |> Async.RunSynchronously)
+            Assert.Equal(uow.PrivateContext.Entry(entity).State, EntityState.Unchanged)
+            entity.Name <- "Changed2"
+            uow.PrivateContext.Update(entity) |> ignore
+            Assert.Equal(uow.PrivateContext.Entry(entity).State, EntityState.Modified)
+            uow.SaveChanges() |> ignore
+            Assert.Equal(uow.PrivateContext.Entry(entity).State, EntityState.Unchanged)
+            Assert.True(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Changed2") |> Async.AwaitTask |> Async.RunSynchronously)
+            raise (Exception("Test exception"))
+        )
+        Assert.False(result.IsSuccess)
+        Assert.False(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Changed2") |> Async.AwaitTask |> Async.RunSynchronously)
+        Assert.False(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Changed1") |> Async.AwaitTask |> Async.RunSynchronously)
+        Assert.False(uow.PrivateContext.Set<DummyEntity>().AnyAsync(fun e -> e.Name = "Test") |> Async.AwaitTask |> Async.RunSynchronously)
+        Assert.Single(uow.PrivateContext.Set<DummyEntity>().Local) |> ignore
+        Assert.Equal(uow.PrivateContext.Entry(entity).State, EntityState.Added)
+        Assert.Equal("Test", entity.Name)
 
 
