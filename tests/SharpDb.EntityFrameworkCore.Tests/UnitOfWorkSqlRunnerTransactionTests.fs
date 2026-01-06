26cdd305c9ba22574e803b6fa4b3fc0c7ca26099
Jan Martinek <jmartinek11@gmail.com>
2026-01-06 13:58:44 +0100

    test(SD.EntityFrameworkCore): Change UoW tests due to DbContext ownership specification

--- tests/SharpDb.EntityFrameworkCore.Tests/UnitOfWorkSqlRunnerTransactionTests.fs
+++ tests/SharpDb.EntityFrameworkCore.Tests/UnitOfWorkSqlRunnerTransactionTests.fs
@@ -1,11 +1,11 @@
 namespace SharpDb.EntityFrameworkCore.Tests
 
 open System
-open System.Threading.Tasks
 open Microsoft.Data.Sqlite
 open Microsoft.EntityFrameworkCore
 open Xunit
 open SharpDb.EntityFrameworkCore
+open System.Reflection
 
 module UnitOfWorkSqlRunnerTransactionTests =
 
@@ -20,108 +20,97 @@ module UnitOfWorkSqlRunnerTransactionTests =
             modelBuilder.Entity<DummyEntity>().HasKey("Id") |> ignore
             modelBuilder.Entity<DummyEntity>().Property(fun e -> e.Id).ValueGeneratedOnAdd().HasColumnName("id") |> ignore
 
-    type DummyUnitOfWork(ctx: DbContext) =
-        inherit UnitOfWork(ctx)
+    type DummyUnitOfWork(ctxFactory: IDbContextFactory<DummyDbContext>) =
+        inherit UnitOfWork<DummyDbContext>(ctxFactory)
+        member this.PrivateContext = this.GetContext()
 
-    let createContextSqlite() =
-        let conn = new SqliteConnection($"Data Source={Guid.NewGuid()};mode=memory;cache=shared;")
-        conn.Open()
-        let options = DbContextOptionsBuilder<DummyDbContext>()
-                        .UseSqlite(conn)
-                        .Options
-        let ctx = new DummyDbContext(options)
-        ctx.Database.EnsureCreated() |> ignore
-        ctx, conn
+    type SqliteContextFactory() =
+        let connection = new SqliteConnection($"Data Source={Guid.NewGuid()};mode=memory;cache=shared;")
+        interface System.IDisposable with
+            member _.Dispose() =
+                if connection.State = System.Data.ConnectionState.Open then
+                    connection.Close()
+                connection.Dispose()
+        interface IDbContextFactory<DummyDbContext> with
+            member _.CreateDbContext() =
+                if connection.State <> System.Data.ConnectionState.Open then
+                    connection.Open()
+                let options = DbContextOptionsBuilder<DummyDbContext>()
+                                .UseSqlite(connection)
+                                .Options
+                let ctx = new DummyDbContext(options)
+                ctx.Database.EnsureCreated() |> ignore
+                ctx
 
     [<Fact>]
     let ``EfcSqlRunner SqlExecuteAsync in UoW transaction commits successfully`` () =
         task {
-            let ctx, conn = createContextSqlite()
-            try
-                let uow = DummyUnitOfWork(ctx)
-                let name = "SqlRunnerTest"
-                let! result = uow.InTransactionAsync(fun () ->
-                    task {
-                        let r2 = uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({name})""").AsTask() |> Async.AwaitTask
-                        let r3 = uow.Sql.ManyAsync<DummyEntity>($"""SELECT * FROM DummyEntity WHERE Name = {name}""", fun r -> new DummyEntity()).AsTask() |> Async.AwaitTask
-                        ignore r2
-                        ignore r3
-                    }
-                )
-                Assert.True(result.IsSuccess)
-                Assert.Equal(1, ctx.Set<DummyEntity>().CountAsync(fun e -> e.Name = name).Result)
-            finally
-                ctx.Dispose()
-                conn.Close()
-                conn.Dispose()
+            use dbContextFactory = new SqliteContextFactory()
+            use uow = new DummyUnitOfWork(dbContextFactory)
+            let name = "SqlRunnerTest"
+            let! result = uow.InTransactionAsync(fun () ->
+                task {
+                    let r2 = uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({name})""").AsTask() |> Async.AwaitTask
+                    let r3 = uow.Sql.ManyAsync<DummyEntity>($"""SELECT * FROM DummyEntity WHERE Name = {name}""", fun r -> new DummyEntity()).AsTask() |> Async.AwaitTask
+                    ignore r2
+                    ignore r3
+                }
+            )
+            Assert.True(result.IsSuccess)
+            Assert.Equal(1, uow.PrivateContext.Set<DummyEntity>().CountAsync(fun e -> e.Name = name).Result)
         }
 
     [<Fact>]
     let ``EfcSqlRunner SqlExecuteAsync in UoW transaction rolls back on exception`` () =
-        let ctx, conn = createContextSqlite()
-        try
-            let uow = DummyUnitOfWork(ctx)
-            let name = "SqlRunnerRollback"
-            let result = uow.InTransaction(fun () ->
-                uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({name})""").AsTask().Wait()
-                raise (Exception("Rollback"))
-            )
-            Assert.False(result.IsSuccess)
-            Assert.Equal(0, ctx.Set<DummyEntity>().CountAsync(fun e -> e.Name = name).Result)
-        finally
-            ctx.Dispose()
-            conn.Close()
-            conn.Dispose()
+        use dbContextFactory = new SqliteContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
+        let name = "SqlRunnerRollback"
+        let result = uow.InTransaction(fun () ->
+            uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({name})""").AsTask().Wait()
+            raise (Exception("Rollback"))
+        )
+        Assert.False(result.IsSuccess)
+        Assert.Equal(0, uow.PrivateContext.Set<DummyEntity>().CountAsync(fun e -> e.Name = name).Result)
 
     [<Fact>]
     let ``Nested UoW transactions with EfcSqlRunner roll back inner only`` () =
-        let ctx, conn = createContextSqlite()
-        try
-            let uow = DummyUnitOfWork(ctx)
-            let nameOuter = "OuterSql"
-            let nameInner = "InnerSql"
-            let result =
-                uow.InTransaction(fun () ->
-                    uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({nameOuter})""").AsTask().Wait()
-                    let innerResult =
-                        uow.InTransaction(fun () ->
-                            uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({nameInner})""").AsTask().Wait()
-                            raise (Exception("Inner rollback"))
-                        )
-                    Assert.False(innerResult.IsSuccess)
-                )
-            Assert.True(result.IsSuccess)
-            let countOuter = ctx.Set<DummyEntity>().CountAsync(fun e -> e.Name = nameOuter).Result
-            let countInner = ctx.Set<DummyEntity>().CountAsync(fun e -> e.Name = nameInner).Result
-            Assert.Equal(1, countOuter)
-            Assert.Equal(0, countInner)
-        finally
-            ctx.Dispose()
-            conn.Close()
-            conn.Dispose()
+        use dbContextFactory = new SqliteContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
+        let nameOuter = "OuterSql"
+        let nameInner = "InnerSql"
+        let result =
+            uow.InTransaction(fun () ->
+                uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({nameOuter})""").AsTask().Wait()
+                let innerResult =
+                    uow.InTransaction(fun () ->
+                        uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({nameInner})""").AsTask().Wait()
+                        raise (Exception("Inner rollback"))
+                    )
+                Assert.False(innerResult.IsSuccess)
+            )
+        Assert.True(result.IsSuccess)
+        let countOuter = uow.PrivateContext.Set<DummyEntity>().CountAsync(fun e -> e.Name = nameOuter).Result
+        let countInner = uow.PrivateContext.Set<DummyEntity>().CountAsync(fun e -> e.Name = nameInner).Result
+        Assert.Equal(1, countOuter)
+        Assert.Equal(0, countInner)
 
     [<Fact>]
     let ``Nested UoW transactions with EfcSqlRunner roll back all on outer exception`` () =
-        let ctx, conn = createContextSqlite()
-        try
-            let uow = DummyUnitOfWork(ctx)
-            let nameOuter = "OuterAllSql"
-            let nameInner = "InnerAllSql"
-            let result =
-                uow.InTransaction(fun () ->
-                    uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({nameOuter})""").AsTask().Wait()
-                    let _ =
-                        uow.InTransaction(fun () ->
-                            uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({nameInner})""").AsTask().Wait()
-                        )
-                    raise (Exception("Outer rollback"))
-                )
-            Assert.False(result.IsSuccess)
-            let countOuter = ctx.Set<DummyEntity>().CountAsync(fun e -> e.Name = nameOuter).Result
-            let countInner = ctx.Set<DummyEntity>().CountAsync(fun e -> e.Name = nameInner).Result
-            Assert.Equal(0, countOuter)
-            Assert.Equal(0, countInner)
-        finally
-            ctx.Dispose()
-            conn.Close()
-            conn.Dispose()
+        use dbContextFactory = new SqliteContextFactory()
+        use uow = new DummyUnitOfWork(dbContextFactory)
+        let nameOuter = "OuterAllSql"
+        let nameInner = "InnerAllSql"
+        let result =
+            uow.InTransaction(fun () ->
+                uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({nameOuter})""").AsTask().Wait()
+                let _ =
+                    uow.InTransaction(fun () ->
+                        uow.Sql.ExecuteAsync($"""INSERT INTO DummyEntity (Name) VALUES ({nameInner})""").AsTask().Wait()
+                    )
+                raise (Exception("Outer rollback"))
+            )
+        Assert.False(result.IsSuccess)
+        let countOuter = uow.PrivateContext.Set<DummyEntity>().CountAsync(fun e -> e.Name = nameOuter).Result
+        let countInner = uow.PrivateContext.Set<DummyEntity>().CountAsync(fun e -> e.Name = nameInner).Result
+        Assert.Equal(0, countOuter)
+        Assert.Equal(0, countInner)
