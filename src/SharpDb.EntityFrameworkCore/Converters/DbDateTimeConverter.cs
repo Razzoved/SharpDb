4c895cd7a64d1a82513704f5dff01cc216748ce8
Jan Martinek <jmartinek11@gmail.com>
2026-01-12 12:47:39 +0100

    feat(SD.EntityFrameworkCore): Add converters for datetime data types

---
+++ src/SharpDb.EntityFrameworkCore/Converters/DbDateTimeConverter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SharpDb.Entities.DataTypes;
+
+namespace SharpDb.EntityFrameworkCore.Converters;
+
+public sealed class DbDateTimeConverter : ValueConverter<DbDateTime, DateTime>
+{
+    public DbDateTimeConverter() : base(
+        v => v.Value.DateTime,
+        v => new(v))
+    { }
+}
+
+public sealed class DbDateTimeToOffsetConverter : ValueConverter<DbDateTime, DateTimeOffset>
+{
+    public DbDateTimeToOffsetConverter() : base(
+        v => v.Value,
+        v => new(v))
+    { }
+}
+
+public sealed class NullableDbDateTimeConverter : ValueConverter<DbDateTime?, DateTime?>
+{
+    public NullableDbDateTimeConverter() : base(
+        v => v.HasValue ? v.Value.Value.DateTime : null,
+        v => v.HasValue ? new(v.Value) : null)
+    { }
+}
+
+public sealed class NullableDbDateTimeToOffsetConverter : ValueConverter<DbDateTime?, DateTimeOffset?>
+{
+    public NullableDbDateTimeToOffsetConverter() : base(
+        v => v.HasValue ? v.Value.Value : null,
+        v => v.HasValue ? new(v.Value) : null)
+    { }
+}
---
+++ src/SharpDb.EntityFrameworkCore/Converters/DbSmallDateTimeConverter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SharpDb.Entities.DataTypes;
+
+namespace SharpDb.EntityFrameworkCore.Converters;
+
+public sealed class DbSmallDateTimeConverter : ValueConverter<DbSmallDateTime, DateTime>
+{
+    public DbSmallDateTimeConverter() : base(
+        v => v.Value.DateTime,
+        v => new(v))
+    { }
+}
+
+public sealed class DbSmallDateTimeToOffsetConverter : ValueConverter<DbSmallDateTime, DateTimeOffset>
+{
+    public DbSmallDateTimeToOffsetConverter() : base(
+        v => v.Value,
+        v => new(v))
+    { }
+}
+
+public sealed class NullableDbSmallDateTimeConverter : ValueConverter<DbSmallDateTime?, DateTime?>
+{
+    public NullableDbSmallDateTimeConverter() : base(
+        v => v.HasValue ? v.Value.Value.DateTime : null,
+        v => v.HasValue ? new(v.Value) : null)
+    { }
+}
+
+public sealed class NullableDbSmallDateTimeToOffsetConverter : ValueConverter<DbSmallDateTime?, DateTimeOffset?>
+{
+    public NullableDbSmallDateTimeToOffsetConverter() : base(
+        v => v.HasValue ? v.Value.Value : null,
+        v => v.HasValue ? new(v.Value) : null)
+    { }
+}
