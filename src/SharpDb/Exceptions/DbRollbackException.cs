022e5549dd4de7a29b9e212766ea96bc51a42237
Jan Martinek <jmartinek11@gmail.com>
2026-04-19 12:50:17 +0200

    feat(SD): Add custom exception for errors occuring during transaction rollbacks

---
+++ src/SharpDb/Exceptions/DbRollbackException.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Common;
+
+namespace SharpDb.Exceptions;
+
+public class DbRollbackException : DbException
+{
+    public DbRollbackException(string originalErrorMessage, Exception e) : base(originalErrorMessage, e) { }
+
+    public override bool IsTransient => InnerException is DbException { IsTransient: true };
+    public override int ErrorCode => InnerException is DbException e ? e.ErrorCode : default;
+}
