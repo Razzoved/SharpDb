5109eb0e9a3ce660d2492908d54290ef65283283
Jan Martinek <jmartinek11@gmail.com>
2026-05-12 22:56:47 +0200

    feat(SD): Add exception extensions for easier DbError handling

---
+++ src/SharpDb/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Common;
+
+namespace SharpDb.Extensions;
+
+public static class ExceptionExtensions
+{
+    public static bool HasDbError(this Exception exception, byte searchDepth = byte.MaxValue)
+    {
+        Exception? currentException = exception;
+        while (currentException is not null && searchDepth-- > 0)
+        {
+            if (currentException is DbException)
+            {
+                return true;
+            }
+            currentException = exception.InnerException;
+        }
+        return false;
+    }
+
+    public static bool HasTransientDbError(this Exception exception, byte searchDepth = byte.MaxValue)
+    {
+        Exception? currentException = exception;
+        while (currentException is not null && searchDepth-- > 0)
+        {
+            if (currentException is DbException { IsTransient: true })
+            {
+                return true;
+            }
+            currentException = exception.InnerException;
+        }
+        return false;
+    }
+}
