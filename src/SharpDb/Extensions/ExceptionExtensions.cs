45a20e23d39eaa5139d12ea7abaa0d5bef78b9c2
Jan Martinek <jmartinek11@gmail.com>
2026-05-13 10:48:33 +0200

    feat(SD): Add extension for easier propagation of failed db results as exceptions
    Reason: Smoother transition of SaveChanges with result objects. This
    extension allows easy drop-in to existing logic based on exceptions.

--- src/SharpDb/Extensions/ExceptionExtensions.cs
+++ src/SharpDb/Extensions/ExceptionExtensions.cs
@@ -31,4 +31,39 @@ public static class ExceptionExtensions
         }
         return false;
     }
+
+    public static TResult ThrowIfFailed<TResult>(this TResult result) where TResult : IDbResult
+    {
+        if (!result.IsSuccess)
+        {
+            switch (result.Error)
+            {
+                case ExceptionDbError error:
+                    throw error.Exception;
+                case StringDbError error:
+                    throw new Exception(error.Message);
+                default:
+                    throw new Exception(result.Error.ToString());
+            }
+        }
+        return result;
+    }
+
+    public static Task<TResult> ThrowIfFailed<TResult>(this Task<TResult> result) where TResult : IDbResult
+    {
+        return result.ContinueWith(task =>
+        {
+            if (task.IsCompletedSuccessfully)
+                return task.Result.ThrowIfFailed();
+            else if (task.IsFaulted && task.Exception is not null)
+                throw task.Exception;
+            else
+                throw new Exception("An error occurred during asynchronous operation.");
+        }, TaskContinuationOptions.ExecuteSynchronously);
+    }
+
+    public static Task<TResult> ThrowIfFailed<TResult>(this ValueTask<TResult> result) where TResult : IDbResult
+    {
+        return result.AsTask().ThrowIfFailed();
+    }
 }
