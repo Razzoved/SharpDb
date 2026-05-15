b41bf0c818cd04623678317cc5bb945ad26d3788
Jan Martinek <jmartinek11@gmail.com>
2026-05-15 10:19:31 +0200

    feat(SD): Add converters for DbMoney

---
+++ src/SharpDb.EntityFrameworkCore/Converters/DbMoneyConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SharpDb.Entities.DataTypes;
+
+namespace SharpDb.EntityFrameworkCore.Converters;
+
+public sealed class DbMoneyConverter : ValueConverter<DbMoney, decimal>
+{
+    public DbMoneyConverter() : base(
+        v => v.Value,
+        v => new(v),
+        new ConverterMappingHints(precision: 19, scale: 4))
+    { }
+}
+
+public sealed class NullableDbMoneyConverter : ValueConverter<DbMoney?, decimal?>
+{
+    public NullableDbMoneyConverter() : base(
+        v => v.HasValue ? v.Value.Value : null,
+        v => v.HasValue ? new(v.Value) : null,
+        new ConverterMappingHints(precision: 19, scale: 4))
+    { }
+}
