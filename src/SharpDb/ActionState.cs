757889b6e74831d2e3aaab6bce72d5e5820259a8
Jan Martinek <jmartinek11@gmail.com>
2026-08-06 21:42:52 +0200

    refactor(SD): Cleanup ActionState

--- src/SharpDb/ActionState.cs
+++ src/SharpDb/ActionState.cs
@@ -2,7 +2,7 @@ using System.Runtime.CompilerServices;
 
 namespace SharpDb;
 
-/// <summmary>
+/// <summary>
 /// Represents the result of an action executed within a transaction.
 /// Depending on the state, transaction may be committed or rolled back.
 /// </summary>
@@ -25,11 +25,22 @@ public readonly struct ActionState
     /// Creates a new instance of <see cref="ActionState"/> indicating failure.
     /// This state indicates that the transaction should be rolled back.
     /// </summary>
-    public static ActionState Abort(IDbError error) => new(error is null || error is NoDbError ? new StringDbError("Unspecified error") : error);
+    /// <param name="error">The error associated with the failure.</param>
+    public static ActionState Abort(IDbError error) => new(error is null or NoDbError ? new StringDbError("Unspecified error") : error);
 
     /// <summary>
     /// Creates a new instance of <see cref="ActionState"/> indicating failure.
     /// This state indicates that the transaction should be rolled back.
     /// </summary>
+    /// <param name="message">A message to be associated with the error.</param>
+    /// <param name="mn">The name of the method where the error occurred. Automatically provided by the compiler. Prepended to message.</param>
+    /// <param name="ln">The line number where the error occurred. Automatically provided by the compiler. Prepended to message.</param>
     public static ActionState Abort(string message, [CallerMemberName] string? mn = null, [CallerLineNumber] int? ln = null) => new(new StringDbError($"{mn?.Trim() ?? ""}[{ln ?? 0}] {message.Trim()}"));
+
+    /// <summary>
+    /// Creates a new instance of <see cref="ActionState"/> indicating failure.
+    /// This state indicates that the transaction should be rolled back.
+    /// </summary>
+    /// <param name="message">A message to be associated with the error. The message is passed as is.</param>
+    public static ActionState AbortRaw(string message) => new(new StringDbError(message));
 }
