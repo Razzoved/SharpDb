bfd551705493732e848788c6045750814e4e7418
Jan Martinek <jmartinek11@gmail.com>
2026-08-06 22:00:27 +0200

    feat(SD)!: ISqlRunner implementations must implement some basic SQL methods
    - single
    - first or default
    - many
    - execute
    
    !!! BREAKING CHANGES !!!
    
    Implementors must check that they comply with new contract definition.
    The change has been made to allow usage of base library in
    non-db-specific contexts (i.e. libraries). Extended functionality is and
    will be still left to specific implementations.

--- src/SharpDb/ISqlRunner.cs
+++ src/SharpDb/ISqlRunner.cs
@@ -1,4 +1,6 @@
-﻿namespace SharpDb;
+﻿using System.Data.Common;
+
+namespace SharpDb;
 
 public interface ISqlRunner
 {
@@ -8,4 +10,16 @@ public interface ISqlRunner
     /// </summary>
     /// <returns>Info object</returns>
     DbConnectionInfo GetConnectionInfo();
+
+    ValueTask<DbQueryResult<T>> SingleAsync<T>(FormattableString sql, Func<DbDataReader, T> reader, CancellationToken cancellation = default);
+    ValueTask<DbQueryResult<T>> RawSingleAsync<T>(string sql, Func<DbDataReader, T> reader, CancellationToken cancellation, params DbParameter[] parameters);
+
+    ValueTask<DbQueryResult<T?>> FirstOrDefaultAsync<T>(FormattableString sql, Func<DbDataReader, T> reader, CancellationToken cancellation = default);
+    ValueTask<DbQueryResult<T?>> RawFirstOrDefaultAsync<T>(string sql, Func<DbDataReader, T> reader, CancellationToken cancellation, params DbParameter[] parameters);
+
+    ValueTask<DbQueryResult<IReadOnlyList<T>>> ManyAsync<T>(FormattableString sql, Func<DbDataReader, T> reader, CancellationToken cancellation = default);
+    ValueTask<DbQueryResult<IReadOnlyList<T>>> RawManyAsync<T>(string sql, Func<DbDataReader, T> reader, CancellationToken cancellation, params DbParameter[] parameters);
+
+    ValueTask<DbExecResult> ExecuteAsync(FormattableString sql, CancellationToken cancellation = default);
+    ValueTask<DbExecResult> RawExecuteAsync(string sql, CancellationToken cancellation, params DbParameter[] parameters);
 }
