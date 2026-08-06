116b95793434b1d63e4f559235827a7531940380
Jan Martinek <jmartinek11@gmail.com>
2026-08-06 22:25:58 +0200

    refactor(SD.EntityFrameworkCore): Prefer primary constructors in converters

--- src/SharpDb.EntityFrameworkCore/Converters/DbDateTimeConverter.cs
+++ src/SharpDb.EntityFrameworkCore/Converters/DbDateTimeConverter.cs
@@ -3,34 +3,18 @@ using SharpDb.Entities.DataTypes;
 
 namespace SharpDb.EntityFrameworkCore.Converters;
 
-public sealed class DbDateTimeConverter : ValueConverter<DbDateTime, DateTime>
-{
-    public DbDateTimeConverter() : base(
-        v => v.Value.DateTime,
-        v => new(v))
-    { }
-}
+public sealed class DbDateTimeConverter() : ValueConverter<DbDateTime, DateTime>(
+    v => v.Value.DateTime,
+    v => new DbDateTime(v));
 
-public sealed class DbDateTimeToOffsetConverter : ValueConverter<DbDateTime, DateTimeOffset>
-{
-    public DbDateTimeToOffsetConverter() : base(
-        v => v.Value,
-        v => new(v))
-    { }
-}
+public sealed class DbDateTimeToOffsetConverter() : ValueConverter<DbDateTime, DateTimeOffset>(
+    v => v.Value,
+    v => new DbDateTime(v));
 
-public sealed class NullableDbDateTimeConverter : ValueConverter<DbDateTime?, DateTime?>
-{
-    public NullableDbDateTimeConverter() : base(
-        v => v.HasValue ? v.Value.Value.DateTime : null,
-        v => v.HasValue ? new(v.Value) : null)
-    { }
-}
+public sealed class NullableDbDateTimeConverter() : ValueConverter<DbDateTime?, DateTime?>(
+    v => v.HasValue ? v.Value.Value.DateTime : null,
+    v => v.HasValue ? new DbDateTime(v.Value) : null);
 
-public sealed class NullableDbDateTimeToOffsetConverter : ValueConverter<DbDateTime?, DateTimeOffset?>
-{
-    public NullableDbDateTimeToOffsetConverter() : base(
-        v => v.HasValue ? v.Value.Value : null,
-        v => v.HasValue ? new(v.Value) : null)
-    { }
-}
+public sealed class NullableDbDateTimeToOffsetConverter() : ValueConverter<DbDateTime?, DateTimeOffset?>(
+    v => v.HasValue ? v.Value.Value : null,
+    v => v.HasValue ? new DbDateTime(v.Value) : null);
