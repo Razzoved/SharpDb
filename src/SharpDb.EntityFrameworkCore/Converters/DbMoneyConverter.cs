d63acb76fec1ef99ffffd0422f645a596908597d
Jan Martinek <jmartinek11@gmail.com>
2026-08-06 22:25:58 +0200

    refactor(SD.EntityFrameworkCore): Prefer primary constructors in converters

--- src/SharpDb.EntityFrameworkCore/Converters/DbMoneyConverter.cs
+++ src/SharpDb.EntityFrameworkCore/Converters/DbMoneyConverter.cs
@@ -3,20 +3,12 @@ using SharpDb.Entities.DataTypes;
 
 namespace SharpDb.EntityFrameworkCore.Converters;
 
-public sealed class DbMoneyConverter : ValueConverter<DbMoney, decimal>
-{
-    public DbMoneyConverter() : base(
-        v => v.Value,
-        v => new(v),
-        new ConverterMappingHints(precision: 19, scale: 4))
-    { }
-}
+public sealed class DbMoneyConverter() : ValueConverter<DbMoney, decimal>(
+    v => v.Value,
+    v => new DbMoney(v),
+    new ConverterMappingHints(precision: 19, scale: 4));
 
-public sealed class NullableDbMoneyConverter : ValueConverter<DbMoney?, decimal?>
-{
-    public NullableDbMoneyConverter() : base(
-        v => v.HasValue ? v.Value.Value : null,
-        v => v.HasValue ? new(v.Value) : null,
-        new ConverterMappingHints(precision: 19, scale: 4))
-    { }
-}
+public sealed class NullableDbMoneyConverter() : ValueConverter<DbMoney?, decimal?>(
+    v => v.HasValue ? v.Value.Value : null,
+    v => v.HasValue ? new DbMoney(v.Value) : null,
+    new ConverterMappingHints(precision: 19, scale: 4));
--- src/SharpDb.EntityFrameworkCore/Converters/DbSmallMoneyConverter.cs
+++ src/SharpDb.EntityFrameworkCore/Converters/DbSmallMoneyConverter.cs
@@ -3,20 +3,12 @@ using SharpDb.Entities.DataTypes;
 
 namespace SharpDb.EntityFrameworkCore.Converters;
 
-public sealed class DbSmallMoneyConverter : ValueConverter<DbSmallMoney, decimal>
-{
-    public DbSmallMoneyConverter() : base(
-        v => v.Value,
-        v => new(v),
-        new ConverterMappingHints(precision: 10, scale: 4))
-    { }
-}
+public sealed class DbSmallMoneyConverter() : ValueConverter<DbSmallMoney, decimal>(
+    v => v.Value,
+    v => new DbSmallMoney(v),
+    new ConverterMappingHints(precision: 10, scale: 4));
 
-public sealed class NullableDbSmallMoneyConverter : ValueConverter<DbSmallMoney?, decimal?>
-{
-    public NullableDbSmallMoneyConverter() : base(
-        v => v.HasValue ? v.Value.Value : null,
-        v => v.HasValue ? new(v.Value) : null,
-        new ConverterMappingHints(precision: 10, scale: 4))
-    { }
-}
+public sealed class NullableDbSmallMoneyConverter() : ValueConverter<DbSmallMoney?, decimal?>(
+    v => v.HasValue ? v.Value.Value : null,
+    v => v.HasValue ? new DbSmallMoney(v.Value) : null,
+    new ConverterMappingHints(precision: 10, scale: 4));
